862e46067d510e6b65b8802b04722ffafbd00603
Always <56392346+Alweys@users.noreply.github.com>
2021-09-22 12:59:31 +0200

    Dvojni kompliment
    (na laksi nacin)

---
+++ 22.9.2021/22.9.2021/Program.cs
@@ -0,0 +1,44 @@
+﻿using System;
+
+namespace _22._9._2021
+{
+    class Program
+    {
+        static string dvojniKompliment(string s)
+        {
+            char[] arr;
+
+            arr = s.ToCharArray();
+            for (int i=0;i<s.Length;i++)
+            {
+                if (arr[i] == '1')
+                    arr[i] = '0';
+                else
+                    arr[i] = '1';
+            }
+
+            for (int j = s.Length-1; j >= 0; j--)
+            {
+                if (arr[j] == '0')
+                {
+                    arr[j] = '1';
+                    break;
+                }
+                else
+                {
+                    arr[j] = '0';
+                    continue;
+                }
+            }
+
+            string result = new string(arr);
+
+            return result;
+        }
+
+        static void Main(string[] args)
+        {
+            Console.WriteLine(dvojniKompliment(Console.ReadLine()));
+        }
+    }
+}
